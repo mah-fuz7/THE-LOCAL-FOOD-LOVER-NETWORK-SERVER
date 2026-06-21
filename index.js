b992b0ee274d565b1b6129e74a0cd36127b3f35a
mah-fuz7 <mahafuz846@gmail.com>
2026-06-21 23:20:19 +0600

    verifytoken

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const app=express();
 const cors=require("cors");
+const admin = require("firebase-admin");
+
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -10,10 +12,47 @@ const port=process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json())
 
+// Firebase
+const decoded=Buffer.from(process.env.FB_SERVICE_KEY,"base64").toString(
+    "utf8"
+);
+
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// firebase verify toke
+const verifyToken=async(req,res,next) =>{
+    const authHeader=req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({
+            message:"Unauthorized"
+        });
+    }
+const token=authHeader.split(" ")[1];
+if(!token){
+    return res.status(401).send({
+        message:"Unauthorized"
+    })
+}
+try{
+    const decoded=await admin.auth().verifyIdToken(token);
+    req.decoded=decoded
+    next()
+}catch{
+    return res.status(403).send({
+        message:"Forbidden"
+    });
+}
+};
+
+
 // mongoDB
-// mongoDB user:TLFLN-SERVER
-// mongoDB User Pass: hnUk0UOIhsn5ANxb
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.due0kmg.mongodb.net/?appName=Cluster0`;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
